b5f21e9a9b58e6866fbde2b2a83817d989d14857
Alexis Brooks <91137432+0xAlexisSys@users.noreply.github.com>
2026-06-17 22:40:21 +0600

    Remove unused strings

--- osu.Game/Localisation/NamedOverlayComponentStrings.cs
+++ osu.Game/Localisation/NamedOverlayComponentStrings.cs
@@ -10,21 +10,6 @@ namespace osu.Game.Localisation
         private const string prefix = @"osu.Game.Resources.Localisation.NamedOverlayComponent";
 
         /// <summary>
-        /// "browse for new beatmaps"
-        /// </summary>
-        public static LocalisableString BeatmapListingDescription => new TranslatableString(getKey(@"beatmap_listing_description"), @"browse for new beatmaps");
-
-        /// <summary>
-        /// "view your friends and spectate other players"
-        /// </summary>
-        public static LocalisableString DashboardDescription => new TranslatableString(getKey(@"dashboard_description"), @"view your friends and spectate other players");
-
-        /// <summary>
-        /// "find out who&#39;s the best right now"
-        /// </summary>
-        public static LocalisableString RankingsDescription => new TranslatableString(getKey(@"rankings_description"), @"find out who's the best right now");
-
-        /// <summary>
         /// "get up-to-date on community happenings"
         /// </summary>
         public static LocalisableString NewsDescription => new TranslatableString(getKey(@"news_description"), @"get up-to-date on community happenings");

972f20ad867b6c34d2e12678da567da7650253be
Alexis Brooks <91137432+0xAlexisSys@users.noreply.github.com>
2026-06-30 18:59:37 +0600

    Remove unused notification strings

--- osu.Game/Localisation/NotificationsStrings.cs
+++ osu.Game/Localisation/NotificationsStrings.cs
@@ -59,11 +59,6 @@ Please try changing your audio device to a working setting.");
         public static LocalisableString ScoreOverlayDisabled(LocalisableString arg0) => new TranslatableString(getKey(@"score_overlay_disabled"), @"The score overlay is currently disabled. You can toggle this by pressing {0}.", arg0);
 
         /// <summary>
-        /// "The URL {0} has an unsupported or dangerous protocol and will not be opened."
-        /// </summary>
-        public static LocalisableString UnsupportedOrDangerousUrlProtocol(string url) => new TranslatableString(getKey(@"unsupported_or_dangerous_url_protocol"), @"The URL {0} has an unsupported or dangerous protocol and will not be opened.", url);
-
-        /// <summary>
         /// "Subsequent messages have been logged. Click to view log files."
         /// </summary>
         public static LocalisableString SubsequentMessagesLogged => new TranslatableString(getKey(@"subsequent_messages_logged"), @"Subsequent messages have been logged. Click to view log files.");
@@ -77,98 +72,6 @@ Please try changing your audio device to a working setting.");
         /// "Encountered tablet warning, your tablet may not function correctly. Click here for a list of all tablets supported."
         /// </summary>
         public static LocalisableString EncounteredTabletWarning => new TranslatableString(getKey(@"encountered_tablet_warning"), @"Encountered tablet warning, your tablet may not function correctly. Click here for a list of all tablets supported.");
-
-        /// <summary>
-        /// "This link type is not yet supported!"
-        /// </summary>
-        public static LocalisableString LinkTypeNotSupported => new TranslatableString(getKey(@"unsupported_link_type"), @"This link type is not yet supported!");
-
-        /// <summary>
-        /// "{0} invited you to the multiplayer match &quot;{1}&quot;! Click to join."
-        /// </summary>
-        public static LocalisableString InvitedYouToTheMultiplayer(string username, string roomName) => new TranslatableString(getKey(@"invited_you_to_the_multiplayer"), @"{0} invited you to the multiplayer match ""{1}""! Click to join.", username, roomName);
-
-        /// <summary>
-        /// "You do not have the beatmap for this replay."
-        /// </summary>
-        public static LocalisableString MissingBeatmapForReplay => new TranslatableString(getKey(@"missing_beatmap_for_replay"), @"You do not have the beatmap for this replay.");
-
-        /// <summary>
-        /// "Downloading missing beatmap for this replay..."
-        /// </summary>
-        public static LocalisableString DownloadingBeatmapForReplay => new TranslatableString(getKey(@"downloading_beatmap_for_replay"), @"Downloading missing beatmap for this replay...");
-
-        /// <summary>
-        /// "Your local copy of the beatmap for this replay appears to be different than expected. You may need to update or re-download it."
-        /// </summary>
-        public static LocalisableString MismatchingBeatmapForReplay => new TranslatableString(getKey(@"mismatching_beatmap_for_replay"), @"Your local copy of the beatmap for this replay appears to be different than expected. You may need to update or re-download it.");
-
-        /// <summary>
-        /// "You are now running osu! {0}.
-        /// Click to see what&#39;s new!"
-        /// </summary>
-        public static LocalisableString GameVersionAfterUpdate(string version) => new TranslatableString(getKey(@"game_version_after_update"), @"You are now running osu! {0}.
-Click to see what's new!", version);
-
-        /// <summary>
-        /// "Update ready to install. Click to restart!"
-        /// </summary>
-        public static LocalisableString UpdateReadyToInstall => new TranslatableString(getKey(@"update_ready_to_install"), @"Update ready to install. Click to restart!");
-
-        /// <summary>
-        /// "This is not an official build of the game. Scores will not be submitted and other online systems may not work as intended."
-        /// </summary>
-        public static LocalisableString NotOfficialBuild => new TranslatableString(getKey(@"not_official_build"), @"This is not an official build of the game. Scores will not be submitted and other online systems may not work as intended.");
-
-        /// <summary>
-        /// "Downloading update..."
-        /// </summary>
-        public static LocalisableString DownloadingUpdate => new TranslatableString(getKey(@"downloading_update"), @"Downloading update...");
-
-        /// <summary>
-        /// "This multiplayer room has ended. Click to display room results."
-        /// </summary>
-        public static LocalisableString MultiplayerRoomEnded => new TranslatableString(getKey(@"multiplayer_room_ended"), @"This multiplayer room has ended. Click to display room results.");
-
-        /// <summary>
-        /// "Mention"
-        /// </summary>
-        public static LocalisableString Mention => new TranslatableString(getKey(@"mention"), @"Mention");
-
-        /// <summary>
-        /// "{0} in {1}"
-        /// </summary>
-        public static LocalisableString MentionDetails(string user, string channelName) => new TranslatableString(getKey(@"mention_details"), @"{0} in {1}", user, channelName);
-
-        /// <summary>
-        /// "Online: {0}"
-        /// </summary>
-        public static LocalisableString FriendOnline(string info) => new TranslatableString(getKey(@"friend_online"), @"Online: {0}", info);
-
-        /// <summary>
-        /// "Offline: {0}"
-        /// </summary>
-        public static LocalisableString FriendOffline(string info) => new TranslatableString(getKey(@"friend_offline"), @"Offline: {0}", info);
-
-        /// <summary>
-        /// "Connection to online services was interrupted. osu! will be operating with limited functionality."
-        /// </summary>
-        public static LocalisableString APIConnectionInterrupted => new TranslatableString(getKey(@"api_connection_interrupted"), @"Connection to online services was interrupted. osu! will be operating with limited functionality.");
-
-        /// <summary>
-        /// "You have been logged out on this device due to a login to your account on another device."
-        /// </summary>
-        public static LocalisableString AnotherDeviceDisconnect => new TranslatableString(getKey(@"another_device_disconnect"), @"You have been logged out on this device due to a login to your account on another device.");
-
-        /// <summary>
-        /// "You have been logged out due to a change to your account. Please log in again."
-        /// </summary>
-        public static LocalisableString AccountChangeDisconnect => new TranslatableString(getKey(@"account_change_disconnect"), @"You have been logged out due to a change to your account. Please log in again.");
-
-        /// <summary>
-        /// "Downloading {0}"
-        /// </summary>
-        public static LocalisableString Downloading(string info) => new TranslatableString(getKey(@"downloading"), @"Downloading {0}", info);
 
         /// <summary>
         /// "Collections import is initialising..."
@@ -194,21 +97,6 @@ Click to see what's new!", version);
         /// "This error has been automatically reported to the dev team."
         /// </summary>
         public static LocalisableString ErrorAutomaticallyReported => new TranslatableString(getKey(@"error_automatically_reported"), @"This error has been automatically reported to the dev team.");
-
-        /// <summary>
-        /// "A newer release of osu! has been found ({0} → {1})."
-        /// </summary>
-        public static LocalisableString UpdateAvailable(string oldVersion, string newVersion) => new TranslatableString(getKey(@"update_available"), @"A newer release of osu! has been found ({0} → {1}).", oldVersion, newVersion);
-
-        /// <summary>
-        /// "Click here to download the new version, which can be installed over the top of your existing installation."
-        /// </summary>
-        public static LocalisableString UpdateAvailableManualInstall => new TranslatableString(getKey(@"update_available_manual_install"), @"Click here to download the new version, which can be installed over the top of your existing installation.");
-
-        /// <summary>
-        /// "Check with your package manager / provider to bring osu! up-to-date!"
-        /// </summary>
-        public static LocalisableString UpdateAvailablePackageManaged => new TranslatableString(getKey(@"update_available_package_managed"), @"Check with your package manager / provider to bring osu! up-to-date!");
 
         /// <summary>
         /// "An action was interrupted due to a dialog being displayed."
@@ -251,26 +139,6 @@ Click to see what's new!", version);
         /// </summary>
         public static LocalisableString ScreenshotSaved(string filename) => new TranslatableString(getKey(@"screenshot_saved"), @"Screenshot saved! Click to view.
 {0}", filename);
-
-        /// <summary>
-        /// "The multiplayer server will be right back..."
-        /// </summary>
-        public static LocalisableString MultiplayerServerShuttingDownImmediately => new TranslatableString(getKey(@"multiplayer_server_shutting_down_immediately"), @"The multiplayer server will be right back...");
-
-        /// <summary>
-        /// "The multiplayer server is restarting in {0}."
-        /// </summary>
-        public static LocalisableString MultiplayerServerShuttingDownRemaining(string remainingTime) => new TranslatableString(getKey(@"multiplayer_server_shutting_down_remaining"), @"The multiplayer server is restarting in {0}.", remainingTime);
-
-        /// <summary>
-        /// "Created new collection &quot;{0}&quot; with {1} beatmaps."
-        /// </summary>
-        public static LocalisableString CollectionCreated(string name, int beatmapsCount) => new TranslatableString(getKey(@"collection_created"), @"Created new collection ""{0}"" with {1} beatmaps.", name, beatmapsCount);
-
-        /// <summary>
-        /// "Added {0} beatmaps to collection &quot;{1}&quot;."
-        /// </summary>
-        public static LocalisableString CollectionBeatmapsAdded(string name, int beatmapsCount) => new TranslatableString(getKey(@"collection_beatmaps_added"), @"Added {0} beatmaps to collection ""{1}"".", beatmapsCount, name);
 
         private static string getKey(string key) => $@"{prefix}:{key}";
     }

7208ab10c5de6dc0695221a0a1ace1da198dace9
Alexis Brooks <91137432+0xAlexisSys@users.noreply.github.com>
2026-07-08 01:45:43 +0600

    Minor BeatmapMetadataInfoExtensions changes
    - Make `Match` method an expression body
    - Remove unused searchable term methods

--- osu.Game/Beatmaps/BeatmapMetadataInfoExtensions.cs
+++ osu.Game/Beatmaps/BeatmapMetadataInfoExtensions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
 using osu.Framework.Localisation;
 using osu.Game.Screens.Select;
 
@@ -9,47 +8,13 @@ namespace osu.Game.Beatmaps
 {
     public static class BeatmapMetadataInfoExtensions
     {
-        internal const int MAX_SEARCHABLE_TERM_COUNT = 7;
-
-        /// <summary>
-        /// An array of all searchable terms provided in contained metadata.
-        /// </summary>
-        public static string[] GetSearchableTerms(this IBeatmapMetadataInfo metadataInfo)
-        {
-            var termsList = new List<string>(MAX_SEARCHABLE_TERM_COUNT);
-            CollectSearchableTerms(metadataInfo, termsList);
-            return termsList.ToArray();
-        }
-
-        public static bool Match(IBeatmapMetadataInfo metadataInfo, FilterCriteria.OptionalTextFilter filter)
-        {
-            if (filter.Matches(metadataInfo.Author)) return true;
-            if (filter.Matches(metadataInfo.Artist)) return true;
-            if (filter.Matches(metadataInfo.ArtistUnicode)) return true;
-            if (filter.Matches(metadataInfo.Title)) return true;
-            if (filter.Matches(metadataInfo.TitleUnicode)) return true;
-            if (filter.Matches(metadataInfo.Source)) return true;
-            if (filter.Matches(metadataInfo.Tags)) return true;
-
-            return false;
-        }
-
-        internal static void CollectSearchableTerms(IBeatmapMetadataInfo metadataInfo, IList<string> termsList)
-        {
-            addIfNotNull(metadataInfo.Author);
-            addIfNotNull(metadataInfo.Artist);
-            addIfNotNull(metadataInfo.ArtistUnicode);
-            addIfNotNull(metadataInfo.Title);
-            addIfNotNull(metadataInfo.TitleUnicode);
-            addIfNotNull(metadataInfo.Source);
-            addIfNotNull(metadataInfo.Tags);
-
-            void addIfNotNull(string s)
-            {
-                if (!string.IsNullOrEmpty(s))
-                    termsList.Add(s);
-            }
-        }
+        public static bool Match(IBeatmapMetadataInfo metadataInfo, FilterCriteria.OptionalTextFilter filter) => filter.Matches(metadataInfo.Author)
+                                                                                                                 || filter.Matches(metadataInfo.Artist)
+                                                                                                                 || filter.Matches(metadataInfo.ArtistUnicode)
+                                                                                                                 || filter.Matches(metadataInfo.Title)
+                                                                                                                 || filter.Matches(metadataInfo.TitleUnicode)
+                                                                                                                 || filter.Matches(metadataInfo.Source)
+                                                                                                                 || filter.Matches(metadataInfo.Tags);
 
         /// <summary>
         /// A user-presentable display title representing this metadata.

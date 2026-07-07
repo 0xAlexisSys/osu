422ca07412874abceaa8b1fe264ddc9a32c9406a
Alexis Brooks <91137432+0xAlexisSys@users.noreply.github.com>
2026-07-07 16:33:10 +0600

    Windows: Remove URI association code
    On one thought: not sure if `WindowsAssociationManager` should be kept or deleted.

--- osu.Desktop/Windows/WindowsAssociationManager.cs
+++ osu.Desktop/Windows/WindowsAssociationManager.cs
@@ -39,8 +39,6 @@ namespace osu.Desktop.Windows
         /// </summary>
         private const string program_id_file_prefix = "osu.File";
 
-        private const string program_id_protocol_prefix = "osu.Uri";
-
         private static readonly ApplicationCapability application_capability = new ApplicationCapability(@"osu-sp", @"Software\ppy\osu-sp\Capabilities", "osu-sp");
 
         private static readonly FileAssociation[] file_associations =
@@ -51,14 +49,8 @@ namespace osu.Desktop.Windows
             new FileAssociation(@".osk", WindowsAssociationManagerStrings.OsuSkin, Icons.Beatmap),
         };
 
-        private static readonly UriAssociation[] uri_associations =
-        {
-            new UriAssociation(@"osu", WindowsAssociationManagerStrings.OsuProtocol, Icons.Lazer),
-            new UriAssociation(@"osump", WindowsAssociationManagerStrings.OsuMultiplayer, Icons.Lazer),
-        };
-
         /// <summary>
-        /// Installs file and URI associations.
+        /// Installs file associations.
         /// </summary>
         /// <remarks>
         /// Call <see cref="LocaliseDescriptions"/> in a timely fashion to keep descriptions up-to-date and localised.
@@ -72,7 +64,7 @@ namespace osu.Desktop.Windows
             }
             catch (Exception e)
             {
-                Logger.Error(e, $@"Failed to install file and URI associations: {e.Message}");
+                Logger.Error(e, $@"Failed to install file associations: {e.Message}");
             }
         }
 
@@ -91,7 +83,7 @@ namespace osu.Desktop.Windows
             }
             catch (Exception e)
             {
-                Logger.Error(e, @"Failed to update file and URI associations.");
+                Logger.Error(e, @"Failed to update file associations.");
             }
         }
 
@@ -105,14 +97,11 @@ namespace osu.Desktop.Windows
                 foreach (var association in file_associations)
                     association.LocaliseDescription(localisationManager);
 
-                foreach (var association in uri_associations)
-                    association.LocaliseDescription(localisationManager);
-
                 NotifyShellUpdate();
             }
             catch (Exception e)
             {
-                Logger.Error(e, @"Failed to update file and URI association descriptions.");
+                Logger.Error(e, @"Failed to update file association descriptions.");
             }
         }
 
@@ -125,14 +114,11 @@ namespace osu.Desktop.Windows
                 foreach (var association in file_associations)
                     association.Uninstall();
 
-                foreach (var association in uri_associations)
-                    association.Uninstall();
-
                 NotifyShellUpdate();
             }
             catch (Exception e)
             {
-                Logger.Error(e, @"Failed to uninstall file and URI associations.");
+                Logger.Error(e, @"Failed to uninstall file associations.");
             }
         }
 
@@ -148,11 +134,7 @@ namespace osu.Desktop.Windows
             foreach (var association in file_associations)
                 association.Install();
 
-            foreach (var association in uri_associations)
-                association.Install();
-
             application_capability.RegisterFileAssociations(file_associations);
-            application_capability.RegisterUriAssociations(uri_associations);
         }
 
         #region Native interop
@@ -215,17 +197,6 @@ namespace osu.Desktop.Windows
 
                 foreach (var association in associations)
                     fileAssociations.SetValue(association.Extension, association.ProgramId);
-            }
-
-            public void RegisterUriAssociations(UriAssociation[] associations)
-            {
-                using var capability = Registry.CurrentUser.OpenSubKey(capabilityPath, true);
-                if (capability is null) return;
-
-                using var urlAssociations = capability.CreateSubKey(@"UrlAssociations");
-
-                foreach (var association in associations)
-                    urlAssociations.SetValue(association.Protocol, association.ProgramId);
             }
 
             public void LocaliseDescription(LocalisationManager localisationManager)
@@ -318,72 +289,6 @@ namespace osu.Desktop.Windows
                 }
 
                 classes.DeleteSubKeyTree(ProgramId, throwOnMissingSubKey: false);
-            }
-        }
-
-        private class UriAssociation
-        {
-            /// <summary>
-            /// "The <c>URL Protocol</c> string value indicates that this key declares a custom pluggable protocol handler."
-            /// See https://learn.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa767914(v=vs.85).
-            /// </summary>
-            private const string url_protocol = @"URL Protocol";
-
-            public string Protocol { get; }
-            private LocalisableString description { get; }
-            private string iconPath { get; }
-
-            public UriAssociation(string protocol, LocalisableString description, string iconPath)
-            {
-                Protocol = protocol;
-                this.description = description;
-                this.iconPath = iconPath;
-            }
-
-            public string ProgramId => $@"{program_id_protocol_prefix}.{Protocol}";
-
-            /// <summary>
-            /// Registers an URI protocol handler in accordance with https://learn.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa767914(v=vs.85).
-            /// </summary>
-            public void Install()
-            {
-                using var classes = Registry.CurrentUser.OpenSubKey(software_classes, true);
-                if (classes is null) return;
-
-                using (var protocolKey = classes.CreateSubKey(Protocol))
-                {
-                    protocolKey.SetValue(null, $@"URL:{description}");
-                    protocolKey.SetValue(url_protocol, string.Empty);
-
-                    // clear out old data
-                    protocolKey.DeleteSubKeyTree(default_icon, throwOnMissingSubKey: false);
-                    protocolKey.DeleteSubKeyTree(@"Shell", throwOnMissingSubKey: false);
-                }
-
-                // register a program id for the given protocol
-                using (var programKey = classes.CreateSubKey(ProgramId))
-                {
-                    using (var defaultIconKey = programKey.CreateSubKey(default_icon))
-                        defaultIconKey.SetValue(null, iconPath);
-
-                    using (var openCommandKey = programKey.CreateSubKey(SHELL_OPEN_COMMAND))
-                        openCommandKey.SetValue(null, $@"""{exe_path}"" ""%1""");
-                }
-            }
-
-            public void LocaliseDescription(LocalisationManager localisationManager)
-            {
-                using var classes = Registry.CurrentUser.OpenSubKey(software_classes, true);
-                if (classes is null) return;
-
-                using (var protocolKey = classes.OpenSubKey(Protocol, true))
-                    protocolKey?.SetValue(null, $@"URL:{localisationManager.GetLocalisedString(description)}");
-            }
-
-            public void Uninstall()
-            {
-                using var classes = Registry.CurrentUser.OpenSubKey(software_classes, true);
-                classes?.DeleteSubKeyTree(ProgramId, throwOnMissingSubKey: false);
             }
         }
     }
--- osu.Game/Localisation/WindowsAssociationManagerStrings.cs
+++ osu.Game/Localisation/WindowsAssociationManagerStrings.cs
@@ -24,16 +24,6 @@ namespace osu.Game.Localisation
         /// </summary>
         public static LocalisableString OsuSkin => new TranslatableString(getKey(@"osu_skin"), @"osu! Skin");
 
-        /// <summary>
-        /// "osu!"
-        /// </summary>
-        public static LocalisableString OsuProtocol => new TranslatableString(getKey(@"osu_protocol"), @"osu!");
-
-        /// <summary>
-        /// "osu! Multiplayer"
-        /// </summary>
-        public static LocalisableString OsuMultiplayer => new TranslatableString(getKey(@"osu_multiplayer"), @"osu! Multiplayer");
-
         private static string getKey(string key) => $@"{prefix}:{key}";
     }
 }
